77cf24c14ef614020ecfa81b6d949148126b674e
Michael Pierce <michael@affektive.com>
2014-05-19 17:48:54 -0600

    Fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
     def index
-    #   @name = params[:name]
-    #   @email = params[:email]
-    #   @message = params[:message]
-    #   if @email?
-    #     ContactMailer.contact_email(@name, @email, @message).deliver
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      if @email?
+        ContactMailer.contact_email(@name, @email, @message).deliver
+      end
     end
 end
